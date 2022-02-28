6b894ff713f254c7c369a12ebe7f70a76cdfcd47
Michael B. McLaughlin <mikebmcl@gmail.com>
2022-02-27 19:07:27 -0500

    Started to add UI Testing tests that reflect the current design of the app. Commented out the tests in Given_FirstView since they reflect when the app was just a pure tech demo that started with FirstView as its initial view and thus don't even work in their present state. Kept them for now because they contain a bit of useful examples and could be revised to work properly (i.e. by navigating to FirstViewModel through whatever mechanism allows it) if FirstView itself is ultimately kept and can be navigated to in some way.

--- ReactiveUIUnoSample.UITest/Given_FirstView.cs
+++ ReactiveUIUnoSample.UITest/Given_FirstView.cs
@@ -1,108 +1,108 @@
-﻿using FluentAssertions.Common;
-using NUnit.Framework;
+﻿//using FluentAssertions.Common;
+//using NUnit.Framework;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-using Uno.UITest.Helpers;
-using Uno.UITests.Helpers;
-using Uno.UITest.Helpers.Queries;
-using ReactiveUI.Testing;
+//using Uno.UITest.Helpers;
+//using Uno.UITests.Helpers;
+//using Uno.UITest.Helpers.Queries;
+//using ReactiveUI.Testing;
 
 
-using Query = System.Func<Uno.UITest.IAppQuery, Uno.UITest.IAppQuery>;
-using FluentAssertions;
-using FluentAssertions.Execution;
-using Uno.UITest;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using ReactiveUIUnoSample.ViewModels;
+//using Query = System.Func<Uno.UITest.IAppQuery, Uno.UITest.IAppQuery>;
+//using FluentAssertions;
+//using FluentAssertions.Execution;
+//using Uno.UITest;
+//using Windows.UI.Xaml;
+//using Windows.UI.Xaml.Controls;
+//using ReactiveUIUnoSample.ViewModels;
 
-namespace ReactiveUIUnoSample.UITest
-{
-    public class Given_FirstView : AppTestBase
-    {
-        // Note: Some testing tools will show you the results using the method name while others prefer a Description, so the Description should be set and both
-        // should be descriptive of what the test is testing and the expected result. We're following the Given-When-Then test pattern
-        // (see: https://martinfowler.com/bliki/GivenWhenThen.html ) and because this is UI testing, the Given should normally be the name of the view we are
-        // going to be testing.
-        [Test(Description = "When the app is queried for elements marked EnteredAmountTextBox then there should be exactly one found.")]
-        public void WhenQueriedForElementsNamedEnteredAmountTextBox_ThenThereShouldBeExactlyOneFound()
-        {
-            var app = App;
-            if (app == null)
-            {
-                AppInitializer.TestEnvironment.CurrentPlatform = Platform.Browser;
-                app = AppInitializer.AttachToApp(AppDataMode.Clear);
-            }
-            else
-            {
-                App.RefreshBrowser();
-            }
-            Query enteredAmountTextBox = q => q.All().Marked(FirstViewModel.EnteredAmountTextBoxAutomationId);
-            //using var _ = new AssertionScope();
-            var matchedElementCount = app.WaitForElement(enteredAmountTextBox).Length;
-            Assert.IsTrue(matchedElementCount == 1);
-        }
+//namespace ReactiveUIUnoSample.UITest
+//{
+//    public class Given_FirstView : AppTestBase
+//    {
+//        // Note: Some testing tools will show you the results using the method name while others prefer a Description, so the Description should be set and both
+//        // should be descriptive of what the test is testing and the expected result. We're following the Given-When-Then test pattern
+//        // (see: https://martinfowler.com/bliki/GivenWhenThen.html ) and because this is UI testing, the Given should normally be the name of the view we are
+//        // going to be testing.
+//        [Test(Description = "When the app is queried for elements marked EnteredAmountTextBox then there should be exactly one found.")]
+//        public void WhenQueriedForElementsNamedEnteredAmountTextBox_ThenThereShouldBeExactlyOneFound()
+//        {
+//            var app = App;
+//            if (app == null)
+//            {
+//                AppInitializer.TestEnvironment.CurrentPlatform = Platform.Browser;
+//                app = AppInitializer.AttachToApp(AppDataMode.Clear);
+//            }
+//            else
+//            {
+//                App.RefreshBrowser();
+//            }
+//            Query enteredAmountTextBox = q => q.All().Marked(FirstViewModel.EnteredAmountTextBoxAutomationId);
+//            //using var _ = new AssertionScope();
+//            var matchedElementCount = app.WaitForElement(enteredAmountTextBox).Length;
+//            Assert.IsTrue(matchedElementCount == 1);
+//        }
 
-        [Test(Description = "When the app is loaded then the value of EnteredAmountTextBox should contain the currency symbol of the converter that is should be used.")]
-        public void WhenLoaded_ThenEnteredAmountShouldBeContainTheExpectedCurrencySymbol()
-        {
-            var app = App;
-            if (app == null)
-            {
-                AppInitializer.TestEnvironment.CurrentPlatform = Platform.Browser;
-                app = AppInitializer.AttachToApp(AppDataMode.Clear);
-            }
-            else
-            {
-                App.RefreshBrowser();
-            }
-            Query enteredAmountTextBox = q => q.All().Marked(FirstViewModel.EnteredAmountTextBoxAutomationId);
-            app.WaitForElement(enteredAmountTextBox);
-            using var _ = new AssertionScope();
-            string result = app.Query(q => enteredAmountTextBox(q).GetDependencyPropertyValue(nameof(TextBox.Text)).Value<string>()).FirstOrDefault();
-            result.Should().Contain(Converters.DecimalToStringBindingTypeConverter.CultureInfoForConversion.NumberFormat.CurrencySymbol, "because the initial value should contain the currency symbol");
-        }
+//        [Test(Description = "When the app is loaded then the value of EnteredAmountTextBox should contain the currency symbol of the converter that is should be used.")]
+//        public void WhenLoaded_ThenEnteredAmountShouldBeContainTheExpectedCurrencySymbol()
+//        {
+//            var app = App;
+//            if (app == null)
+//            {
+//                AppInitializer.TestEnvironment.CurrentPlatform = Platform.Browser;
+//                app = AppInitializer.AttachToApp(AppDataMode.Clear);
+//            }
+//            else
+//            {
+//                App.RefreshBrowser();
+//            }
+//            Query enteredAmountTextBox = q => q.All().Marked(FirstViewModel.EnteredAmountTextBoxAutomationId);
+//            app.WaitForElement(enteredAmountTextBox);
+//            using var _ = new AssertionScope();
+//            string result = app.Query(q => enteredAmountTextBox(q).GetDependencyPropertyValue(nameof(TextBox.Text)).Value<string>()).FirstOrDefault();
+//            result.Should().Contain(Converters.DecimalToStringBindingTypeConverter.CultureInfoForConversion.NumberFormat.CurrencySymbol, "because the initial value should contain the currency symbol");
+//        }
 
-        [Test(Description = "When valid pure numeric value then successful formatting.")]
-        public void WhenEnteredAmountTextBoxGetsValidPureNumericValue_ThenSuccessfulFormatting()
-        {
-            var app = App;
-            if (app == null)
-            {
-                AppInitializer.TestEnvironment.CurrentPlatform = Platform.Browser;
-                app = AppInitializer.AttachToApp(AppDataMode.Clear);
-            }
-            else
-            {
-                App.RefreshBrowser();
-            }
-            Query enteredAmountTextBox = q => q.All().Marked(FirstViewModel.EnteredAmountTextBoxAutomationId);
-            app.WaitForElement(enteredAmountTextBox);
-            decimal value = 100000.32M;
-            app.Query(q => enteredAmountTextBox(q).SetDependencyPropertyValue(nameof(TextBox.Text), value.ToString()));
-            // This switches focus away from the text box so that the value conversion that is hooked up to it will run.
-            app.EnterText(enteredAmountTextBox, OpenQA.Selenium.Keys.Tab);
+//        [Test(Description = "When valid pure numeric value then successful formatting.")]
+//        public void WhenEnteredAmountTextBoxGetsValidPureNumericValue_ThenSuccessfulFormatting()
+//        {
+//            var app = App;
+//            if (app == null)
+//            {
+//                AppInitializer.TestEnvironment.CurrentPlatform = Platform.Browser;
+//                app = AppInitializer.AttachToApp(AppDataMode.Clear);
+//            }
+//            else
+//            {
+//                App.RefreshBrowser();
+//            }
+//            Query enteredAmountTextBox = q => q.All().Marked(FirstViewModel.EnteredAmountTextBoxAutomationId);
+//            app.WaitForElement(enteredAmountTextBox);
+//            decimal value = 100000.32M;
+//            app.Query(q => enteredAmountTextBox(q).SetDependencyPropertyValue(nameof(TextBox.Text), value.ToString()));
+//            // This switches focus away from the text box so that the value conversion that is hooked up to it will run.
+//            app.EnterText(enteredAmountTextBox, OpenQA.Selenium.Keys.Tab);
 
-            app.Wait(1);
-            // Take a screenshot and add it to the test results
-            TakeScreenshot("successfulformatting");
-            using var _ = new AssertionScope();
-            string result = app.Query(q => enteredAmountTextBox(q).GetDependencyPropertyValue(nameof(TextBox.Text)).Value<string>()).FirstOrDefault();
-            result.Should().NotBeNullOrWhiteSpace($"because {FirstViewModel.EnteredAmountTextBoxAutomationId} should not be null or empty after setting its value and switching focus away from it");
-            var converter = new Converters.DecimalToStringBindingTypeConverter();
-            if (!converter.TryConvert(value, typeof(string), null, out object expected) || !(expected is string))
-            {
-                Assert.Fail($"value {value} should have converted successfully to a string.");
-            }
-            else
-            {
-                result.Should().Be(expected as string, $"because of conversion using {nameof(Converters.DecimalToStringBindingTypeConverter)}");
-            }
-        }
-    }
-}
+//            app.Wait(1);
+//            // Take a screenshot and add it to the test results
+//            TakeScreenshot("successfulformatting");
+//            using var _ = new AssertionScope();
+//            string result = app.Query(q => enteredAmountTextBox(q).GetDependencyPropertyValue(nameof(TextBox.Text)).Value<string>()).FirstOrDefault();
+//            result.Should().NotBeNullOrWhiteSpace($"because {FirstViewModel.EnteredAmountTextBoxAutomationId} should not be null or empty after setting its value and switching focus away from it");
+//            var converter = new Converters.DecimalToStringBindingTypeConverter();
+//            if (!converter.TryConvert(value, typeof(string), null, out object expected) || !(expected is string))
+//            {
+//                Assert.Fail($"value {value} should have converted successfully to a string.");
+//            }
+//            else
+//            {
+//                result.Should().Be(expected as string, $"because of conversion using {nameof(Converters.DecimalToStringBindingTypeConverter)}");
+//            }
+//        }
+//    }
+//}
--- ReactiveUIUnoSample.UITest/Given_TemperatureConversionsMainView.cs
+++ ReactiveUIUnoSample.UITest/Given_TemperatureConversionsMainView.cs
@@ -20,11 +20,33 @@ using Uno.UITest;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using ReactiveUIUnoSample.ViewModels;
+using ReactiveUIUnoSample.ViewModels.Testing;
+using ReactiveUIUnoSample.ViewModels.UnitConversions;
 
 namespace ReactiveUIUnoSample.UITest
 {
     public class Given_TemperatureConversionsMainView : AppTestBase
     {
+        [TestCase(Description = "When Loaded, Then there should be exactly one control with AutomationId TempInputAutomationId")]
+        public void WhenLoaded_ThenThereShouldBeExactlyOneControlWithAutomationIdTempInputAutomationId()
+        {
+            App.RefreshBrowser();
+            Query tempInputControl = q => q.All().Marked(TemperatureConversionsViewModel.TempInputAutomationId);
+            var matchedElementCount = App.WaitForElement(tempInputControl).Length;
+            Assert.That(matchedElementCount, Is.EqualTo(1));
+        }
+
+        [TestCase(Description = "When Loaded, Then there should be exactly one control with AutomationId TempConversionResultAutomationId"
+            // To change the base name that will be used for screenshot names from the method name to something you choose explicitly, set TestName = "..." such as below
+            , TestName = "VerifyOnlyOneTempConversionResultAutomationIdControl"
+            )]
+        public void WhenLoaded_ThenThereShouldBeExactlyOneControlWithAutomationIdTempConversionResultAutomationId()
+        {
+            App.RefreshBrowser();
+            Query someControl = q => q.All().Marked(TemperatureConversionsViewModel.TempConversionResultAutomationId);
+            var matchedElementCount = App.WaitForElement(someControl).Length;
+            Assert.That(matchedElementCount, Is.EqualTo(1));
+        }
 
     }
 }
